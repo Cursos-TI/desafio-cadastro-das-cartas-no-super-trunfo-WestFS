5b9099564faa2f9c5e481a716d127844737ce1f5
West <135288316+WestFS@users.noreply.github.com>
2025-06-15 19:15:12 -0300

    fix CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {        
     // <!--- VARIAVEIS DA CARTA 1 --->
     char estado_1;
